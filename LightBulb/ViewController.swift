aecf5bb17aacb156807bfac21eab465e1cb6a0cd
Raoul Koopman <raoul.koopman@gmail.com>
2018-04-05 14:45:20 -0400

    working code, missing link to UI

--- LightBulb/ViewController.swift
+++ LightBulb/ViewController.swift
@@ -12,24 +12,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var lightBulb: UIImageView!
     
-    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
-        lightBulb.backgroundColor = UIColor.blue
-        
+        lightBulb.backgroundColor = UIColor.cyan
     }
-    
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
-        
-        print(sender.selectedSegmentIndex)
-        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            lightBulb.backgroundColor = UIColor.red
+        case 1:
+            lightBulb.backgroundColor = UIColor.yellow
+        case 2:
+            lightBulb.backgroundColor = UIColor.blue
+        case 3:
+            lightBulb.backgroundColor = UIColor.green
+        default:
+            lightBulb.backgroundColor = UIColor.cyan
+        }
     }
-    
-    
-
-   
-
 }
